8a69bbc6b84b7bdf618841a1a1eeaea5ec646d4c
DocasDev <jhnmarlenn@gmail.com>
2023-09-15 18:06:05 -0300

    Adaptação para GuzzleHttp 7.2

--- config/laravel-moodle.php
+++ config/laravel-moodle.php
@@ -3,5 +3,5 @@
 return [
     'url' => env('LM_URL', 'https://url-to-moodle-service.com'),
     'token' => env('LM_TOKEN', 'Y0uR!tOken'),
-    'format' => 'RESPONSE_FORMAT_JSON' // Options [RESPONSE_FORMAT_JSON | RESPONSE_FORMAT_XML]
+    'format' => 'json' // Options [json | xml]
 ];
